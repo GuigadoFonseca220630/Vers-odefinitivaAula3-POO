06eacd954686390431b341ebc8028ac57c2b95ad
GuigadoFonseca220630 <g.f.oliva@uni9.edu.br>
2025-03-12 17:35:31 -0300

    Atualizar o Program.cs
    manutenção

--- aula-03/Program.cs
+++ aula-03/Program.cs
@@ -3,28 +3,29 @@ using aula_03;
 // Criando a instância da classe Televisao
 Televisao tvSala = new Televisao(22f);
 
-Console.WriteLine($"O tamanho da TV é: {tvSala.Tamanho}");
-Console.WriteLine($"O volume inicial da TV é: {tvSala.Volume}");
+Console.WriteLine($"Tamanho da TV: {tvSala.Tamanho}");
+Console.WriteLine($"Volume inicial: {tvSala.Volume}");
 
+// Aumentar volume
 tvSala.AumentarVolume();
-Console.WriteLine($"O volume da TV após aumento é: {tvSala.Volume}");
+Console.WriteLine($"Volume após aumento: {tvSala.Volume}");
 
 // Ativar modo mudo
 tvSala.AlternarModoMudo();
 Console.WriteLine("TV no modo mudo.");
 
-// Aumentar volume mesmo no modo mudo
+// Tentar aumentar volume no modo mudo
 tvSala.AumentarVolume();
-Console.WriteLine($"O volume da TV (ainda no modo mudo) é: {tvSala.Volume}");
+Console.WriteLine($"Volume (em modo mudo): {tvSala.Volume}");
 
 // Desativar modo mudo
 tvSala.AlternarModoMudo();
-Console.WriteLine($"O volume da TV após desativar o mudo é: {tvSala.Volume}");
+Console.WriteLine($"Volume após desativar mudo: {tvSala.Volume}");
 
 // Diminuir volume
 tvSala.DiminuirVolume();
-Console.WriteLine($"O volume da TV após diminuir é: {tvSala.Volume}");
+Console.WriteLine($"Volume após diminuir: {tvSala.Volume}");
 
 // Subir canal
 tvSala.SubirCanal();
-Console.WriteLine($"Canal atual da TV: {tvSala.Canal}");
+Console.WriteLine($"Canal atual: {tvSala.Canal}");
