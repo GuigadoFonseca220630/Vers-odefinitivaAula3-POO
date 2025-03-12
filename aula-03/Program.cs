6cd40051023f6199b795ea1b018a5026a893b47c
GuigadoFonseca220630 <g.f.oliva@uni9.edu.br>
2025-03-12 17:32:06 -0300

    Atualizar o Program.cs
    retirada de partes inuteis

--- aula-03/Program.cs
+++ aula-03/Program.cs
@@ -1,45 +1,30 @@
-﻿using aula_03;
+using aula_03;
 
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
+// Criando a instância da classe Televisao
 Televisao tvSala = new Televisao(22f);
-Console.WriteLine($"O tamanho da tv é: {tvSala.Tamanho}");
 
-Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
-tvSala.AumentarVolume();
-
-Console.WriteLine($"O tamanho da tv é: {tvSala.Tamanho}");
-
+Console.WriteLine($"O tamanho da TV é: {tvSala.Tamanho}");
+Console.WriteLine($"O volume inicial da TV é: {tvSala.Volume}");
 
 tvSala.AumentarVolume();
-Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
-tvSala.AumentarVolume();
+Console.WriteLine($"O volume da TV após aumento é: {tvSala.Volume}");
 
-//Deveria imprimir mudo
+// Ativar modo mudo
 tvSala.AlternarModoMudo();
+Console.WriteLine("TV no modo mudo.");
 
-//Deveria imprimir 01
+// Aumentar volume mesmo no modo mudo
 tvSala.AumentarVolume();
-Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+Console.WriteLine($"O volume da TV (ainda no modo mudo) é: {tvSala.Volume}");
 
-//Deveria imprimir mudo
+// Desativar modo mudo
 tvSala.AlternarModoMudo();
+Console.WriteLine($"O volume da TV após desativar o mudo é: {tvSala.Volume}");
 
-//Deveria imprimir volume 01
-tvSala.AlternarModoMudo();
-
+// Diminuir volume
 tvSala.DiminuirVolume();
-Console.WriteLine($"O volume da tv é: {tvSala.Volume}");
+Console.WriteLine($"O volume da TV após diminuir é: {tvSala.Volume}");
 
-//Deveria imprimir volume 01
-tvSala.AlternarModoMudo();
-
-
-tvSala.subircanal();
-Console.WriteLine($"canal da tv: {tvSala.canalsobedesce}");
-
-
-
-
-
+// Subir canal
+tvSala.SubirCanal();
+Console.WriteLine($"Canal atual da TV: {tvSala.Canal}");
